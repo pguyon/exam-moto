155088656bd2cda0d31da993493b7fed6a237bcf
pguyon <pguyon69@gmail.com>
2021-06-17 12:32:07 +0200

    up

---
+++ Vue/admin/administration.php
@@ -0,0 +1,21 @@
+<html>
+
+<head>
+    <?php
+    require 'Vue/Parts/global-stylesheet.php';
+    ?>
+</head>
+
+<body>
+    <?php
+    require 'Vue/admin/menuadmin.php';
+    ?>
+
+
+</body>
+
+<?php
+require 'Vue/Parts/global.js.php'
+?>
+
+</html>
