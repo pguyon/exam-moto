ec13c4b53bf8bfc810ee0599a13d9eb4e617c2d8
pguyon <pguyon69@gmail.com>
2021-06-17 13:14:57 +0200

    up

--- Vue/moto/addmoto.php
+++ Vue/moto/addmoto.php
@@ -10,15 +10,15 @@
 <body>
     <div class="wrapper fadeInDown">
         <div id="formContent">
-            <h2>Me connecter</h2>
+            <h2>Ajout de moto</h2>
             <form method="post" enctype="multipart/form-data">
                 <input type="text" id="marque" class="fadeIn second" name="marque" placeholder="Entrez la marque de la moto">
                 <input type="text" id="model" class="fadeIn third" name="model" placeholder="Entrez le model de la moto">
                 <select class="form-select" aria-label="Default select example" name="type">
                     <option selected>Enduro</option>
-                    <option value="1">Custom</option>
-                    <option value="2">Sportive</option>
-                    <option value="3">Roadster</option>
+                    <option value="cutom">Custom</option>
+                    <option value="sportive">Sportive</option>
+                    <option value="roadster">Roadster</option>
                 </select>
                 <input type="file" class="form-control" aria-describedby="modeleHelp" name="image" placeholder="Image">
                 <input type="submit" class="fadeIn fourth" value="Ajouter">
