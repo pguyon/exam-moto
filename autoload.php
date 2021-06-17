bcaa5d16b387781a91900021716386fec2bacd6b
pguyon <pguyon69@gmail.com>
2021-06-17 10:12:10 +0200

    autoload

---
+++ autoload.php
@@ -0,0 +1,14 @@
+<?php
+spl_autoload_register(function ($class_name) {
+    $folders = [
+        'Model/Class/',
+        'Model/Manager/',
+        'Controller/',
+    ];
+    foreach ($folders as $folder) {
+        if (file_exists($folder . $class_name . 'php')) {
+            require $folder . $class_name . 'php';
+            return;
+        }
+    }
+});
